5a55ec2d77f0cbf0a643bb591e6a7b8cac53b14c
Etka Kocak <ek223zf@student.lnu.se>
2023-09-19 10:33:55 +0200

    created draft for Item

--- app/src/main/java/model/Item.java
+++ app/src/main/java/model/Item.java
@@ -1,5 +1,77 @@
 package model;
 
+import java.util.Date;
+
 public class Item {
-    
+    private String itemId;
+    private String category;
+    private String name;
+    private String description;
+    private Date creationDate;
+    private int costPerDay;
+    private Member owner;
+
+    public Item(String category, String name, String description, int costPerDay, Member owner) {
+        this.category = category;
+        this.name = name;
+        this.description = description;
+        this.costPerDay = costPerDay;
+        this.owner = owner;
+        this.creationDate = new Date();
+        generateItemId();
+    }
+
+    // This is draft, should be implemented
+
+    private String generateItemId() {
+        return "ABC123"; 
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public int getCostPerDay() {
+        return costPerDay;
+    }
+
+    public void setCostPerDay(int costPerDay) {
+        this.costPerDay = costPerDay;
+    }
+
+    public Member getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Member owner) {
+        this.owner = owner;
+    }
 }
