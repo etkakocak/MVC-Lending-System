8ff602d5670e8cc4862b8442ceb5c7b21c231caa
Sanaa Abdoulkader <sa225hg@student.lnu.se>
2023-09-19 10:58:03 +0200

    created User interface

---
+++ app/src/main/java/model/User.java
@@ -0,0 +1,12 @@
+package model;
+
+public interface User {
+    String getID();
+    void setUsername();
+    String getUsername();
+    void setPassword();
+    String getPassword();
+    Void setRoll();
+    String getRoll();
+
+}
