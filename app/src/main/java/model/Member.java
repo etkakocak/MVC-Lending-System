d62d54b91f84ce9743a597140d98293c57fca596
Aiman Daeef <ad223jd@student.lnu.se>
2024-01-22 12:58:36 +0100

    Fixed the member class

--- app/src/main/java/model/Member.java
+++ app/src/main/java/model/Member.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * This is encaptulation class for members.
@@ -14,14 +16,16 @@ public class Member {
   private int credits;
   private ArrayList<Item> ownedItems = new ArrayList<>();
   private final Time currentDate;
+  private static Set<String> uniqueEmails = new HashSet<>();
+  private static Set<Integer> uniquemobileNos = new HashSet<>();
 
   /**
    * The Member class.
    */
   protected Member(String name, String memberId, String email, int mobile, Time currentDate) {
     this.name = name;
-    this.email = email;
-    this.mobile = mobile;
+    this.setEmail(email);
+    this.setMobile(mobile);
     this.currentDate = currentDate;
     this.creationDate = this.currentDate.getDate();
     this.credits = 0;
@@ -44,16 +48,30 @@ public class Member {
     return email;
   }
 
+  /**
+   * setting email unique.
+   */
   public void setEmail(String email) {
+    if (uniqueEmails.contains(email)) {
+      throw new IllegalArgumentException("Email must be unique");
+    }
     this.email = email;
+    uniqueEmails.add(email);
   }
 
   public int getMobile() {
     return mobile;
   }
 
+  /**
+   * setting mobile number unique.
+   */
   public void setMobile(int mobile) {
+    if (uniquemobileNos.contains(mobile)) {
+      throw new IllegalArgumentException("Mobile must be unique");
+    }
     this.mobile = mobile;
+    uniquemobileNos.add(mobile);
   }
 
   public int getCreationDate() {
@@ -72,9 +90,16 @@ public class Member {
     return new ArrayList<>(ownedItems);
   }
 
-  public void addOwnedItem(Item item) {
-    this.ownedItems.add(item);
-    addCredits(100);
+  /**
+   * If member tries add same item.
+   */
+  public void addOwnedItem(Item newitem) {
+    if (this.ownedItems.stream().anyMatch(item -> item.getName().equals(newitem.getName()))) {
+      throw new IllegalArgumentException("Member already has a ownership for the given Item");
+    } else {
+      this.ownedItems.add(newitem);
+      addCredits(100);
+    }
   }
 
   public void removeOwnedItem(Item item) {
