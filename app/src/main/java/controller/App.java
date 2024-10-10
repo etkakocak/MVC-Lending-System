f990c9e63955dc4f4239ab3e333de252924427e2
Aiman Daeef <ad223jd@student.lnu.se>
2024-10-10 13:15:13 +0200

    App done

--- app/src/main/java/controller/App.java
+++ app/src/main/java/controller/App.java
@@ -6,22 +6,19 @@ import view.ConsoleUi;
 import view.ViewInterface;
 
 /**
- * Responsible for staring the application.
+ * Responsible for starting the application.
  */
 public class App {
   /**
    * Application starting point.
-
+   *
    * @param args command line arguments.
    */
   public static void main(String[] args) {
-    // adapt to start the application in your way
     Time time = new Time();
-    Service m = new Service(time);
-    ViewInterface v = new ConsoleUi();
-    // = new SwedishConsoleUi(); (för svenska verison)
-    ObjectController ctrl = new ObjectController(m, v, time);
-    // = new SwedishController(m, v, time); (för svenska verison)
-    ctrl.start();
+    Service model = new Service(time);
+    ViewInterface view = new ConsoleUi();
+    ObjectController controller = new ObjectController(model, view, time);
+    controller.start();
   }
-} 
+}
